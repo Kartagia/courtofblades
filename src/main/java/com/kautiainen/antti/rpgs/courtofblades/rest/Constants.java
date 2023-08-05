a38c0b145ef67f665ab9fae51ad4a33ec6cf9bbf
Antti Kautiainen <kautsu@elisanet.fi>
2023-08-05 21:08:55 +0300

    fix: adding missing Configuration
    * Added the missing Configuration file.

---
+++ src/main/java/com/kautiainen/antti/rpgs/courtofblades/rest/Constants.java
@@ -0,0 +1,69 @@
+package com.kautiainen.antti.rpgs.courtofblades.rest;
+
+public class Constants
+{
+	/**
+	 * The definitions of the initialization parameters.
+	 */
+	public class Init 
+	{
+		// TODO: Add implements of thethe interfaces expansion of the wanted Initialization constants
+
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+		// The server configuration initialization constants
+		/////////////////////////////////////////////////////////////////////////////////////////
+		/**
+		 * The default executor thread pool size.
+		 */
+		public static final String DEFAULT_EXECUTOR_THREAD_POOL_SIZE = "20";
+
+		/**
+		 * THe default host properoty.
+		 */
+		public static final String DEFAULT_HOST = "http://localhost";
+
+		/**
+		 * The port property.
+		 */
+		public static final String PORT_PROPERTY = "port";
+	
+		/**
+		 * The server host property.
+		 */
+		public static final String HOST_PROPERTY = "hostname";
+
+		/**
+		 * The base URL property.
+		 */
+		public static final String BASE_URL_PROPERTY = "base.url";
+	
+		/**
+		 * The executor thread pool size property.
+		 */
+		public static final String EXECUTOR_THREAD_POOL_SIZE = "executor.threadPool.size";
+
+			// TODO: Add server related initialization
+		}
+	
+	/**
+	 * These define the URL parmaeters used in the route definition strings (e.g. '{userId}').
+	 */
+	public class Url
+	{
+		// TODO: Add implements of the interfaces of the wanted Url constants
+
+		// TODO: Add server related Url constants
+	}
+
+	/**
+	 * These define the route names used in naming each route definitions.  These names are used
+	 * to retrieve URL patterns within the controllers by name to create links in responses.
+	 */
+	public class Routes
+	{
+		// TODO: Add implements of the interfaces of the wanted Routes constants
+
+		// TODO: Add server related Route constants
+	}
+}
